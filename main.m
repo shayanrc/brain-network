8800363ec910a93da0674a22a937a7b679185e22
xion <xion@LucidSphere>
2015-03-03 09:54:19 +0530

    Initital commit of brain network

---
+++ main.m
@@ -0,0 +1,54 @@
+function [err grad output]= main()
+  nnfunctions;
+  inputfunctions;
+  
+  learningRate=1;
+  img=normalize(double(imread("input.jpg")));
+  inputMat=makeImgInputs(img,4);
+
+  inputLayer.weights=randn(961,16); %961 neurons with 16 inputs each
+  inputLayer.grad=zeros(1,961);
+  
+  layer2.weights=randn(16,961); %16 neurons with 961 inputs each
+  layer2.grad=zeros(1,16);
+  
+  %brainLayer : interconnected layers , each layer has 3 neurons with 3*16 inputs
+  brainLayer(1).weights=(6,6*16);  
+  brainLayer(1).grad=zeros(1,6);
+  brainLayer(2).weights=(6,6*16);  
+  brainLayer(2).grad=zeros(1,6);
+  brainLayer(3).weights=(6,6*16);  
+  brainLayer(3).grad=zeros(1,6);
+  
+  outputLayer.weights=randn(121,16);
+  outputLayer.grad=zeros(1,121);
+  
+  brainLayerIO=; %matrix containing the output of each brainLayer; also serves as the input for all brainLayers
+  
+  
+  
+  InputProcessorOutputs=rowInputForward(inputLayer.weights,inputMat,@sigmoid);
+  
+  
+  layer2Outputs=forward (layer2.weights,InputProcessorOutputs,@softmax);
+  
+  
+  desiredOutputs=zeros(size(layer2Outputs));
+  %errors=zeros(1,961);
+  [err grad]=sigmoidCostFunction (layer2Outputs,desiredOutputs);
+  output=layer2Outputs;
+  layer2.grad=grad;
+  %imagesc(inputLayer.grad);
+  inputLayer.grad=backpropagate(layer2.grad,layer2.weights);
+  %pause;
+  
+  %imagesc(inputLayer.grad);
+  
+  %imagesc(layer2.weights);
+  %pause;
+  
+  layer2.weights=modifyWeights(learningRate,layer2.grad,layer2.weights,sigmoidDerivative(layer2Outputs),InputProcessorOutputs);
+  
+  %imagesc(layer2.weights);
+  
+endfunction
---
+++ nnfunctions.m
@@ -0,0 +1,82 @@
+%script file containing NN functions
+1; 
+
+%==============Activation functions==============%
+
+function g = sigmoid(z)
+% sigmoid Compute sigmoid function
+% J = SIGMOID(z) computes the sigmoid of z.
+% z can be a scalar, a vector, or a matrix.
+g = zeros(size(z));
+g = 1 ./ (1 + exp(-z));
+endfunction
+
+function mu = softmax(eta)
+  s=sum(exp(eta));
+  mu = exp(eta)./s;
+endfunction
+
+
+%==============Derivatives of Activation functions==============%
+
+function x=sigmoidDerivative(y)
+
+  x=y.*(1-y);
+endfunction
+
+%==============Cost functions==============
+
+function [cost partGrad]= sigmoidCostFunction(outputs,desiredOutputs)
+%sigmoidCostFunction: function to calculate cost function and part of the gradient for sigmoid activation
+%outputs : outputs of the network
+%desiredOutputs : desired Outputs of the network
+cost =-desiredOutputs.* log(outputs) - (1 - desiredOutputs).* log(1-outputs);
+partGrad=desiredOutputs-outputs;
+endfunction
+
+
+%==============Forward propagation functions==============
+
+function outputs =forward(inputs,weights,activation)
+% forward: generic forward propagation function
+% inputs: input vector to the layer 
+% weights: the weights matrix (no. of rows = no of neurons in layer & no. of columns = no. of inputs)
+% no of columns of inputs and weights must be equal  outputs=weights*inputs';
+  outputs=weights*inputs';
+  outputs=activation(outputs);
+endfunction
+
+function outputs = rowInputForward(inputs,weights,activation)
+% rowInputForward: forward propagation function where each row of input matrix is fed to one neuron
+% inputs: input vector to the layer 
+% weights: the weights matrix (no. of rows = no of neurons in layer & no. of columns = no. of inputs)
+% no of columns of inputs and weights must be equal
+  outputs=forward(inputs,weights,activation);
+  outputs=sum(outputs.*eye(size(outputs)));
+endfunction
+
+
+%==============Backpropagation functions==============
+%these function(s) propagate the error/gradient backwards
+%note: this function should be called for a layer before modifyWeights
+function prevLayerGrad=backpropagate(partGrad,weights)
+  prevLayerGrad=partGrad*weights;
+endfunction
+
+%==============Modify Weights functions==============
+%
+
+function changedweights = modifyWeights(learningRate, costDerivative, weights, activationDerivative, inputs)
+  %modifyWeights : function to modifiy weights
+  %learningRate : learning rate of the network (scalar)
+  %weights : weights matrix of a layer
+  %partGrad : derivative of the cost function / propagated error
+  weightChanges=learningRate*costDerivative.*activationDerivative;
+  weightChanges=weightChanges'*inputs;
+  colorbar;
+  imagesc(weights);
+  
+  changedweights=weights+weightChanges;
+  
+  %imagesc(weights);
+endfunction
